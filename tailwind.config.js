59506743e28f4ca46b684e83108583abf9cc4740
Wahyu Triono <whytrno@gmail.com>
2024-04-03 14:00:19 +0700

    Fix: Make theme default dark

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    daisyui: {
+        themes: ["dark"],
+    },
+    theme: {
+        extend: {},
+    },
+    plugins: [require("daisyui")],
 }
